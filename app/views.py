0ec4164c2787f06003f75abb34c556138a15c782
Viktor Mishyn <viktormishyn@gmail.com>
2020-11-30 20:49:04 +0200

    Created global_variables() function in views.py

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
 from app import app
 
+# ========= SUPPLEMENTARY =========
+
+@app.context_processor
+def global_variables():
+    return dict(COMPANY_NAME="Dunder Mifflin Paper Company Inc. Scranton Branch departments")
+
+
+
+
+
+# ========= VIEWS ==============
 
 @app.route('/')
 @app.route('/departments')
 def departments():
-    return 'departments app'
+    return
 
