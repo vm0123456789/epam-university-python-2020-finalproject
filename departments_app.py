bc5dbe9c854333365ba888349326663f9a196d40
Viktor Mishyn <viktormishyn@gmail.com>
2020-11-30 15:27:19 +0200

    Added shell_context_processor decorator for 'flask shell' to departments_app.py

--- departments_app.py
+++ departments_app.py
@@ -1,2 +1,7 @@
-from app import app
+from app import app, db
+from app.models import Employee, Department
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Employee': Employee, 'Department': Department}
