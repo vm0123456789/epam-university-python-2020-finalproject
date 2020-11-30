68c83274aec4c81b71a424bcc2789f22f1a511c9
Viktor Mishyn <viktormishyn@gmail.com>
2020-11-30 21:35:04 +0200

    Created get_all_departments() function in views.py

--- app/views.py
+++ app/views.py
@@ -14,20 +14,27 @@ def global_variables():
     return dict(COMPANY_NAME="Dunder Mifflin Paper Company Inc. Scranton Branch departments")
 
 
+# ========== API REQUESTS FUNCTIONS ============
+
+def get_all_departments():
+    """
+    :return: departments data in json format: <'id', 'name', 'emploees', 'average_salary'>
+    """
+    return requests.get(f'{BASE}/api/departments').json()
+
+
 # ========= VIEWS ==============
 
 @app.route('/')
 @app.route('/departments')
 def departments():
-    departments = requests.get(f'{BASE}/api/departments').json()
     return render_template('departments.html', title=global_variables()['COMPANY_NAME'],
-                           departments=departments)
+                           departments=get_all_departments())
 
 
 @app.route('/departments/<string:dep_name>')
 def department(dep_name):
-    name = dep_name.capitalize()
-    return ""
+    return render_template('department.html', title=dep_name.capitalize(), departments=get_all_departments())
 
 
 
