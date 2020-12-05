08a00ed8b49b6515e19c03596665da011efa8cdf
Viktor Mishyn <viktormishyn@gmail.com>
2020-12-05 09:52:33 +0200

    Add logging in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,5 +19,21 @@ restServ.add_resource(DepartmentByIdApi, '/api/departments/<int:dep_id>')
 restServ.add_resource(SearchApi, '/api/search')
 restServ.add_resource(SearchByDepartmentApi, '/api/search/<int:dep_id>')
 
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/departments_app.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('departments_app')
+
 from app import views, errors
 
