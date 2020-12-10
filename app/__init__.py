e2e8fc0b55cf40638a57e86bee7801ad9da8336e
viktormishyn <viktormishyn@gmail.com>
2020-12-11 01:21:08 +0200

    Create TestingConfig class in config.py, Add testing mode to create_app() in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import Config
+from config import Config, TestingConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
@@ -12,9 +12,17 @@ import os
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_class=Config):
+
+def create_app(testing=False):
+
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    # testing mode (by default False)
+    if testing:
+        app.config.from_object(TestingConfig)
+    else:
+        app.config.from_object(Config)
+
     from app.models import Employee, Department
     db.init_app(app)
     migrate.init_app(app, db)
--- config.py
+++ config.py
@@ -5,7 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '-very-very-SECRET-KEY-'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
+class TestingConfig(object):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app/tests/test.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
